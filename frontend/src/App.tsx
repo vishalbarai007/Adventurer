01bf90f6fd42b500b6b6a707e39cea53cc3c800a
VISHAL13 <vishalbaraiofficial01@gmail.com>
2024-08-10 14:58:31 +0530

    added button for testing.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,15 @@
 import './App.css'
 import { Button } from './components/ui/button'
+import { motion } from "framer-motion"
 
 function App() {
   return (
 <>
-<Button> hello</Button>
+<motion.button animate={{ x: 100 , y:100}} whileHover={{ scale: 1.2 }}
+  whileTap={{ scale: 1.1 }}
+  drag
+  dragConstraints={{ left: -100, right: 100, top:100, bottom:-100 }}>click & drag me</motion.button>
+<Button> Used MUI</Button>
 </>
   )
 }
