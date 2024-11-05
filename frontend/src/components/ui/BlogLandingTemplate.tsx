acb373375859647546ad2f0bbd7dbcc20a6350ab
vbytex <vishalbaraiofficial01@gmail.com>
2024-11-05 10:36:28 +0530

    BlogsLandingTemplate Updated.

--- frontend/src/components/ui/BlogLandingTemplate.tsx
+++ frontend/src/components/ui/BlogLandingTemplate.tsx
@@ -1,18 +1,21 @@
-
 const BlogLandingTemplate = () => {
   return (
     <>
-        <div className="h-screen w-full bg-zinc-800 justify-center align-middle">
-            <h2 className="text-white">
-                Adventurer
-            </h2>
-            <h1 className="text-white">
-                Perfect Place for your Adventure.
-            </h1>
+      <div className="BlogsLanding h-screen w-full flex items-center justify-center bg-zinc-800">
+        <div className="text-center">
+          <h2 className="text-white text-2xl font-semibold text-stroke">
+            Adventurer
+          </h2>
+          <h1
+            className="text-white text-5xl font-bold text-stroke"
+            data-aos="fade-right"
+          >
+            Perfect Place for your Adventure.
+          </h1>
         </div>
-    
+      </div>
     </>
-  )
-}
+  );
+};
 
 export default BlogLandingTemplate;
--- frontend/src/pages/Blogs.tsx
+++ frontend/src/pages/Blogs.tsx
@@ -1,10 +1,20 @@
+import { useEffect } from "react";
 import BlogLandingTemplate from "../components/ui/BlogLandingTemplate";
 import BlogsData from "../components/ui/BlogsData";
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
+
 
 const Blogs = () => {
+
+  useEffect(() => {
+    AOS.init({ duration: 800 });
+  }, []);
+
   return (
    <>
-      <BlogLandingTemplate/>
+      <BlogLandingTemplate data-aos = "fade-up"/>
       <BlogsData/>
 
    </> 
